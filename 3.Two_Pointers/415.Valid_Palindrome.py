e2eb51fc984a27189ad62c0da96e0ece841cf918
fudigit <30851222+fudigit@users.noreply.github.com>
2019-03-03 22:38:00 -0600

    Update 415.Valid_Palindrome.py

--- 3.Two_Pointers/415.Valid_Palindrome.py
+++ 3.Two_Pointers/415.Valid_Palindrome.py
@@ -3,28 +3,30 @@ class Solution:
     @param s: A string
     @return: Whether the string is a valid palindrome
     """
-    def isPalindrome(self, s):
+        
         # r ++ and l++ if not alnum, require right < left during r++ and l--
         # only compare lower case!
+        
+    def isPalindrome(self, s):
         
         if s is None:
             return False
         
-        right, left = 0, len(s) - 1
+        left, right = 0, len(s) - 1
         
-        while right < left:
+        while left < right:
             
-            while not s[right].isalnum() and right < left:
-                right += 1
-            while not s[left].isalnum() and right < left:
-                left -= 1
+            while not s[left].isalnum() and left < right:
+                left += 1
+            while not s[right].isalnum() and left < right:
+                right -= 1
             
-            if s[right].lower() != s[left].lower():
+            if s[left].lower() != s[right].lower():
                 return False
             
             else:
-                right += 1
-                left -= 1
+                left += 1
+                right -= 1
         
         return True
         
