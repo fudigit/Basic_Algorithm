a086d2b401b7c9df22b275d2ae13976a521b77e5
fudigit <30851222+fudigit@users.noreply.github.com>
2019-03-05 00:12:39 -0600

    Update 521.Remove_Duplicate_Numbers_in_Array.py

--- 3.Two_Pointers/521.Remove_Duplicate_Numbers_in_Array.py
+++ 3.Two_Pointers/521.Remove_Duplicate_Numbers_in_Array.py
@@ -4,10 +4,13 @@ class Solution:
     @return: the number of unique integers
     """
     def deduplication(self, nums):
+        
         # 1. sort the data first! So the same numbers are together!
         # 2. slow pointer points to the last unique integer, fast pointer loop through the
         #    flat/increasing integers. 
         # 3. If fast > slow integer, Let fast integer be the new last unique integer, slow ++
+        
+        ''' O(nlog(n)) + O(n)'''
         
         if nums == []:
             return 0
