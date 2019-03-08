4d519a236b8c9f6486d0c36dd098cba955d3a3df
fudigit <30851222+fudigit@users.noreply.github.com>
2019-03-07 22:53:37 -0600

    Update 464.Sort_Integers_2.py

--- 3.Two_Pointers/464.Sort_Integers_2.py
+++ 3.Two_Pointers/464.Sort_Integers_2.py
@@ -62,5 +62,63 @@ class Solution:
         self.quickSort(A, left, end)
             
         
+class Solution:
+    """
+    @param A: an integer array
+    @return: nothing
+    """
+    '''merge sort'''
+    #repeatedly: 
+    #  1. split the interval from the middle into 2 
+    #  2. merge 2 intervals
+    
+    def sortIntegers2(self, A):
+        # tmp is only used in the merge function to store sorted integers
+        # Save time to define it once, and pass as a parameter in the function
+        tmp = [None]*(len(A))
+        self.mergeSort(A, 0, len(A) - 1, tmp)
+        
+    def mergeSort(self, A, start, end, tmp):
+        
+        #base case
+        if start >= end:
+            return
+            
+        mid = (start + end)//2
+    
+        self.mergeSort(A, start, mid, tmp)
+        self.mergeSort(A, mid + 1, end, tmp)
+        self.merge(A, start, mid, end, tmp)
+    
+    def merge(self, A, start, mid, end, tmp):
+        left_i, right_i = start, mid + 1
+        # A and tmp use the same index, so tmp is copy of A with an sorted interval 
+        index = start
+        
+        while left_i <= mid and right_i <= end:
+            if A[left_i] <= A[right_i]:
+                tmp[index] = (A[left_i])
+                left_i += 1
+            elif A[left_i] > A[right_i]:
+                tmp[index] = (A[right_i])
+                right_i += 1
+            index += 1
+        
+        while left_i <= mid:
+            tmp[index] = (A[left_i])
+            left_i += 1
+            index += 1
+            
+        while right_i <= end:
+            tmp[index] = (A[right_i])
+            right_i += 1
+            index += 1
+        
+        # update array A within merge function, A stored merged result.
+        # tmp stores the 2 merged sorted list, everytime
+        for i in range(start, end+1):
+            A[i] = tmp[i]
+    
+    
         
     
