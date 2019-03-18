15e8f46ffac26acccb5b3281ea866402f19807e9
fudigit <30851222+fudigit@users.noreply.github.com>
2019-03-18 16:21:53 -0500

    Update 5.Kth_Largest_Element.py

--- 3.Two_Pointers/5.Kth_Largest_Element.py
+++ 3.Two_Pointers/5.Kth_Largest_Element.py
@@ -1,3 +1,10 @@
+'''
+quickSelect: return the actual value once kth largest is found
+1. sort the array in asending order
+2. len(A) - k is the index of kth largest number
+note: after partition, kth largest many belong to left, right inteval, or stand along by itself
+'''
+
 class Solution:
     """
     @param n: An integer
@@ -5,36 +12,36 @@ class Solution:
     @return: the Kth largest element
     """
     def kthLargestElement(self, n, nums):
-        
+        # cases of k that are not considered
+        if n > len(nums) or n <= 0:
+            return -1
         
         return self.quickSelect(n, nums, 0, len(nums)-1)
     
-    
     def quickSelect(self, k, nums, start, end):
-        # base case
-        if start >= end:
+        # base case, would start be greater than end?
+        if start == end:
             return nums[start]
         
         # perform a partition with 2 pointers
         l, r = start, end
         pivot = nums[(start+end)//2]
-        
+
         while l <= r:
             while l <= r and nums[l] < pivot:
                 l += 1
             while l <= r and nums[r] > pivot:
                 r -= 1
-            while l <= r:
+            if l <= r:
                 # nums[l] >= pivot and nums[r] <= pivot, swap so both go to the correct sides
-                nums[l] nums[r] = nums[r], nums[l]
+                nums[l], nums[r] = nums[r], nums[l]
                 l, r = l + 1, r - 1
-        print (nums, l, r)
+            print(nums, 'left',l,'right',r, 'start', start, 'end', end, 'k', k)
         if end - k + 1 >= l:
             # if k is in the left interval, assuming asending array
             return self.quickSelect(k, nums, l, end)
         elif end - k + 1 <= r:
             return self.quickSelect(k - (end - r), nums, start, r)
-            
+        # when there is element between l and r
         return nums[r+1]
-        ,
-        # write your code here
+        
