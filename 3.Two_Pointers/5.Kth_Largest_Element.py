a5812055e0b9f2fe0f30448cd5aec5d886dfa5be
fudigit <30851222+fudigit@users.noreply.github.com>
2019-03-18 22:43:11 -0500

    Update 5.Kth_Largest_Element.py

--- 3.Two_Pointers/5.Kth_Largest_Element.py
+++ 3.Two_Pointers/5.Kth_Largest_Element.py
@@ -51,5 +51,58 @@ class Solution:
     
     
     
+'''
+Use 0 as the reference point for k. 
+1. sort the array in asending order
+2. Transform "finding kth largest" to "finding element has index = (len(A) - k)"
+use 0 as the reference point for k. (k = 1, index = len(A) - 1).
+3. return the actual value once kth largest is found
+note: after partition, kth largest many belong to left, right inteval, or bewteen in the 2
+'''
+
+class Solution:
+    """
+    @param n: An integer
+    @param nums: An array
+    @return: the Kth largest element
+    """
+    def kthLargestElement(self, n, nums):
+        # k is bonded by len(nums) and 1
+        if n > len(nums) and n < 1:
+            return -1
+        
+        # finding kth largest is equavelent of finding index of len(A) - k
+        return self.partition(len(nums) - n, nums, 0, len(nums) - 1)
+        
+    def partition(self, k, nums, start, end):
+        # start <= k <= end
+        # base case
+        if start == end:
+            return nums[k]
+        
+        l, r = start, end
+        pivot = nums[(start+end)//2]
+        
+        # partition in asending order
+        while l <= r:
+            while l <= r and nums[l] < pivot:
+                l += 1
+            while l <= r and nums[r] > pivot:
+                r -= 1
+            if l <= r:
+                nums[l], nums[r] = nums[r], nums[l]
+                l, r = l+1, r-1
+        print(nums,l,r)
+        
+        # see which interval k falls in, can be in left, right interval, or right in the middle
+        if  k <= r:
+            return self.partition(k, nums, start, r)
+        elif k >= l:
+            return self.partition(k, nums, l, end)
+        # r < k < l
+        return nums[k]
+        
+    
+    
     
     
