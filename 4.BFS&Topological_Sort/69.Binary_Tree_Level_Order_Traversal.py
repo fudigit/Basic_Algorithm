8b0bbf26b87769611fb86539cd32d77d7292f548
fudigit <30851222+fudigit@users.noreply.github.com>
2019-03-20 00:26:07 -0500

    Create 69.Binary_Tree_Level_Order_Traversal.py

---
+++ 4.BFS&Topological_Sort/69.Binary_Tree_Level_Order_Traversal.py
@@ -0,0 +1,36 @@
+"""
+Definition of TreeNode:
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
+"""
+from collections import deque
+class Solution:
+    """
+    @param root: A Tree
+    @return: Level order a list of lists of integer
+    """
+    def levelOrder(self, root):
+        if root is None:
+            return []
+        
+        # initial of queue
+        queue = deque([root])
+        # hold the result
+        result = []
+        
+        # for each level, expand the next level
+        while queue:
+            level = []
+            for _ in range(len(queue)):
+                node = queue.popleft()
+                level.append(node.val)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            
+            result.append(level)
+            
+        return result
