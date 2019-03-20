f43fdd1adec6591bb4e6dc38b51c4acd1c377a8d
fudigit <30851222+fudigit@users.noreply.github.com>
2019-03-20 15:17:59 -0500

    Update 69.Binary_Tree_Level_Order_Traversal.py

--- 4.BFS&Topological_Sort/69.Binary_Tree_Level_Order_Traversal.py
+++ 4.BFS&Topological_Sort/69.Binary_Tree_Level_Order_Traversal.py
@@ -2,7 +2,10 @@
 For each level, add value for each node, and expand the son nodes for the next level
 1. Use deque to build a queue, right in, left out
 
+Time: Loop through n nodes, therefore O(n)
+Space: Store n nodes in queue, therefore O(n)
 '''
+
 
 """
 Definition of TreeNode:
