9fd128fa6ee9293f596509c80114c05c4ebe5646
fudigit <30851222+fudigit@users.noreply.github.com>
2019-03-25 22:17:36 -0500

    Update 97.Maximum_Depth_of_Binary_Tree.py

--- 5.Binary_Tree&Tree-based_DFS/97.Maximum_Depth_of_Binary_Tree.py
+++ 5.Binary_Tree&Tree-based_DFS/97.Maximum_Depth_of_Binary_Tree.py
@@ -31,3 +31,35 @@ class Solution:
         self.depth = max(self.depth, curDepth)
         self.traverse(node.left, curDepth + 1)
         self.traverse(node.right, curDepth + 1)
+
+        
+'''
+use devide and conquer
+# 1. the depth of method node can be derived from the depth of left and right  child node: motherDepth = max(leftDepth, rightDepth) + 1
+# 2. must return something!
+# 3. similar to post order
+'''
+
+"""
+Definition of TreeNode:
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
+"""
+
+class Solution:
+    """
+    @param root: The root of binary tree.
+    @return: An integer
+    """
+    def maxDepth(self, root):
+        # base case:
+        if root == None:
+            return 0
+        
+        lDepth = self.maxDepth(root.left)
+        rDepth = self.maxDepth(root.right)
+        motherDepth = max(lDepth, rDepth) + 1
+        
+        return motherDepth
