d6ce7b595172d4fa05c1ab7b07c8d06c13cfa4e3
fudigit <30851222+fudigit@users.noreply.github.com>
2019-03-28 15:28:11 -0500

    Update 453.Flatten_Binary_Tree_to_Linked_List.py

--- 5.Binary_Tree&Tree-based_DFS/453.Flatten_Binary_Tree_to_Linked_List.py
+++ 5.Binary_Tree&Tree-based_DFS/453.Flatten_Binary_Tree_to_Linked_List.py
@@ -1,4 +1,40 @@
 '''
+in place
+# trim the right branch, so linking node to the right of last_node does not change right branch 
+'''
+
+"""
+Definition of TreeNode:
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
+"""
+
+class Solution:
+    """
+    @param root: a TreeNode, the root of the binary tree
+    @return: nothing
+    """
+    last_node = None
+    
+    def flatten(self, root):
+        if root == None:
+            return
+        
+        if self.last_node != None:
+            self.last_node.left = None
+            self.last_node.right = root
+            
+        self.last_node = root
+        right = root.right
+        self.flatten(root.left)
+        self.flatten(right)
+
+
+
+
+'''
 Create a new linked list using new TreeNodes
 1. create a dummy node and a cursor node points to dummy node
 2. preorder traverse the binary tree:
