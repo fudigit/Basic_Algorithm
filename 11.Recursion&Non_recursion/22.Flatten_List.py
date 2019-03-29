9cc05c09ee697bf2e8c379a1c0e0ae5486c01fd0
fudigit <30851222+fudigit@users.noreply.github.com>
2019-03-29 15:24:13 -0500

    Update 22.Flatten_List.py

--- 11.Recursion&Non_recursion/22.Flatten_List.py
+++ 11.Recursion&Non_recursion/22.Flatten_List.py
@@ -27,3 +27,33 @@ class Solution(object):
                 self.res.append(item)
             
         return self.res
+
+    
+'''
+Use stack
+pop out the top of the stack
+1. if top is a list, release elements, put back to stack
+2. if top is an integer, append to the result
+stop when stack is []
+
+'''
+
+class Solution(object):
+
+    # @param nestedList a list, each element in the list 
+    # can be a list or integer, for example [1,2,[1,2]]
+    # @return {int[]} a list of integer
+    def flatten(self, nestedList):
+        
+        stack = [nestedList]
+        flattened = []
+        
+        while stack:
+            top = stack.pop()
+            if isinstance(top, list):
+                for item in top:
+                    stack.append(item)
+            elif isinstance(top, list) is False:
+                flattened.append(top)
+        flattened.reverse()
+        return flattened
