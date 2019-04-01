6cc4051db074ce125169c50a1d20bdc79ee93e07
fudigit <30851222+fudigit@users.noreply.github.com>
2019-03-31 23:00:50 -0500

    Create 900.Closest_Binary_Search_Tree_Value.py

---
+++ 5.Binary_Tree&Tree-based_DFS/900.Closest_Binary_Search_Tree_Value.py
@@ -0,0 +1,64 @@
+'''
+non recursion, find upper and lower of target
+upper = biggest value <= target
+lower = smallest value > target
+'''
+
+"""
+Definition of TreeNode:
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
+"""
+
+class Solution:
+    """
+    @param root: the given BST
+    @param target: the given target
+    @return: the value in the BST that is closest to the target
+    """
+    def closestValue(self, root, target):
+        if not root:
+            return root
+            
+        upper = self.find_upper(root, target)
+        lower = self.find_lower(root, target)
+        
+        if upper is None:
+            return lower
+        if lower is None:
+            return upper
+        
+        if target - upper >= lower - target:
+            return lower
+        else:
+            return upper
+    
+        
+    def find_upper(self, root, target):
+        cur_node = root
+        last_node = None
+        while root:
+            if target >= root.val:
+                last_node = root
+                root = root.right
+            else:
+                root = root.left
+        if last_node: #last_node is None if all root.val > target
+            return last_node.val
+        return None
+    
+        
+    def find_lower(self, root, target):
+        cur_node = root
+        last_node = None
+        while root:
+            if target < root.val:
+                last_node = root
+                root = root.left
+            else:
+                root = root.right
+        if last_node:
+            return last_node.val
+        return None
