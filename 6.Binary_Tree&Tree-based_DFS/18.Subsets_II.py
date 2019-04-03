d2a5c2782f8790ab61a331a9834cf4598b5ccabd
fudigit <30851222+fudigit@users.noreply.github.com>
2019-04-02 23:32:16 -0500

    Create 18.Subsets_II.py

---
+++ 6.Binary_Tree&Tree-based_DFS/18.Subsets_II.py
@@ -0,0 +1,37 @@
+'''
+recursion
+some iterations from 17.Subsuets will add duplicates, how to avoid?
+1. within each recursion (same tree level), appending the same element (2nd time) shall be avoided
+2. for a new recursion(another tree level), always add the first element, no matter dup or not.
+'''
+
+class Solution:
+    """
+    @param nums: A set of numbers.
+    @return: A list of lists. All valid subsets.
+    """
+    def subsetsWithDup(self, nums):
+        results = []
+        if nums is None:
+            return results
+        
+        nums.sort()
+        self.counter = 0
+        self.dfs(nums, 0, [], results)
+        return results
+        
+    # 找到所有以subset开始的子集，加入到result
+    def dfs(self, nums, startIndex, subset, results):
+        results.append(subset[:])
+        
+        for i in range(startIndex, len(nums), 1):
+            self.counter += 1
+            print(self.counter)
+            # i > startIndex: so that nums[startIndex] (first of next recursion) is added
+            # since start of next recursion is adding additional elements: [2].append(2)
+            if i != 0 and nums[i] == nums[i-1] and i > startIndex:
+                continue
+            
+            subset.append(nums[i])
+            self.dfs(nums, i + 1, subset, results)
+            subset.pop()
