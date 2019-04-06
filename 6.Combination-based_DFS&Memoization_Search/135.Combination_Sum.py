a1a2ef92eba6f636051a00448ff71d0ed4957bab
fudigit <30851222+fudigit@users.noreply.github.com>
2019-04-06 18:03:54 -0500

    Update 135.Combination_Sum.py

--- 6.Combination-based_DFS&Memoization_Search/135.Combination_Sum.py
+++ 6.Combination-based_DFS&Memoization_Search/135.Combination_Sum.py
@@ -7,21 +7,26 @@ class Solution:
     def combinationSum(self, candidates, target):
         candidates = sorted(list(set(candidates)))
         results = []
-        
+        #self.level = 0      #check tree depth
         self.dfs(candidates, target, 0, [], results)
         return results
         
     def dfs(self, candidates, target, start, combination, results):
+        #self.level += 1
         if target == 0:
-            results.append(list(combination))
+            return results.append(list(combination)) # return, so no need to start another loop
             
         for i in range(start, len(candidates)):
+            #print('i=', i, 'candi=',candidates[i], 'target=', target, 'start=', start, combination, 'level=', self.level)
+
             if target < candidates[i]:
                 return
             
             combination.append(candidates[i])
             self.dfs(candidates, target - candidates[i], i, combination, results)
             combination.pop()
+            #self.level -= 1
+        
 
 
 class Solution:
