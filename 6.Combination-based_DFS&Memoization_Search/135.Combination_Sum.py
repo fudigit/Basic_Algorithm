d2981ad6ab278697f7ff85f43afc819aa3148c65
fudigit <30851222+fudigit@users.noreply.github.com>
2019-04-06 21:29:28 -0500

    Update 135.Combination_Sum.py

--- 6.Combination-based_DFS&Memoization_Search/135.Combination_Sum.py
+++ 6.Combination-based_DFS&Memoization_Search/135.Combination_Sum.py
@@ -26,37 +26,3 @@ class Solution:
             self.dfs(candidates, target - candidates[i], i, combination, results)
             combination.pop()
             #self.level -= 1
-        
-
-
-class Solution:
-    """
-    @param candidates: A list of integers
-    @param target: An integer
-    @return: A list of lists of integers
-    """
-    def combinationSum(self, candidates, target):
-        candidates = sorted(list(set(candidates)))
-        results = []
-        self.level = 0
-        return self.dfs(candidates, target, 0, [], results)
-
-        return results
-        
-    def dfs(self, candidates, target, start, combination, results):
-        self.level += 1
-        if target == 0:
-            return results.append(list(combination))
-    
-        for i in range(start, len(candidates)):
-            print('i=', i, 'candi=',candidates[i], 'target=', target, 'start=', start, combination, 'level=', self.level)
-        
-            if target < candidates[i]:
-                return
-            
-            
-            
-            combination.append(candidates[i])
-            self.dfs(candidates, target - candidates[i], i, combination, results)
-            combination.pop()
-            self.level -= 1
