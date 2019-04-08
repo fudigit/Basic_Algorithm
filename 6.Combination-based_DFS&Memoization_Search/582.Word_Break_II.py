0fe4d3ea768e0a63518cff0ef384d024b50d4bd7
fudigit <30851222+fudigit@users.noreply.github.com>
2019-04-08 18:49:00 -0500

    Create 582.Word_Break_II.py

---
+++ 6.Combination-based_DFS&Memoization_Search/582.Word_Break_II.py
@@ -0,0 +1,53 @@
+'''
+use divide and conquer, similar to finding binary tree path
+1. find all paritions given the current s
+2. to see if a partition is valid, get the prefix and check if it's in the dict
+3. memo search to avoid dups
+
+- exist condition: be careful when partition go towards end of the string
+- why memory can avoid the dups?
+'''
+
+class Solution:
+    """
+    @param: s: A string
+    @param: wordDict: A set of words.
+    @return: All possible sentences.
+    """
+    def wordBreak(self, s, wordDict):
+        memo = {}
+        partitions = self.dfs(s, wordDict, memo)
+        return partitions
+    
+    # find all valid partitions of s, and return all the paritions
+    def dfs(self, s, wordDict, memo):
+        if s in memo:
+            return memo[s]
+        
+        if s == []:
+            return []
+        
+        partitions = []
+        
+        # when shall i starts? deal loop if i starts at 0
+        for i in range(1, len(s)):
+            prefix = s[:i]
+            #print(prefix, partitions)
+            if prefix not in wordDict:
+                continue
+            
+            # if found the prefix in dict, what's all the partitions in s[i:]?
+            sub_partitions = self.dfs(s[i:], wordDict, memo)
+            #print(sub_partitions)
+            #sub_partitions are found, what should the paritions look like? Separate by " "
+            for sub_partition in sub_partitions:
+                #print(prefix,'prefix',sub_partition)
+                partitions.append(prefix + " " + sub_partition)
+        
+        # necessary step: other paritions has no initial string (last char not used)
+        if s in wordDict:
+            partitions.append(s)
+        
+        memo[s] = partitions
+        
+        return partitions
