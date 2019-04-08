0cb345185ea2dfc00e1676bcb47a2a507df107b8
fudigit <30851222+fudigit@users.noreply.github.com>
2019-04-08 16:31:31 -0500

    Update 135.Combination_Sum.py

--- 6.Combination-based_DFS&Memoization_Search/135.Combination_Sum.py
+++ 6.Combination-based_DFS&Memoization_Search/135.Combination_Sum.py
@@ -1,3 +1,37 @@
+'''
+1. use range(1, n + 1) as the list of candidates
+2. find the subset (without reapeating) using dfs
+3. constraint: return when there are k elements 
+
+'''
+
+class Solution:
+    """
+    @param n: Given the range of numbers
+    @param k: Given the numbers of combinations
+    @return: All the combinations of k numbers out of 1..n
+    """
+    def combine(self, n, k):
+        results = []
+        if n is None or k is None:
+            return results
+        
+        self.dfs(n, k, 1, [], results)
+        return results
+        
+    # when combination has k elements, add all combinations start with 'start' to results
+    def dfs(self, n, k, start, combination, results):
+        # deep copy
+        if k == 0:
+            results.append(list(combination))
+            return
+        
+        for i in range(start, n + 1, 1):
+            combination.append(i)
+            self.dfs(n, k - 1, i + 1, combination, results)
+            combination.pop()
+            
+
 class Solution:
     """
     @param candidates: A list of integers
