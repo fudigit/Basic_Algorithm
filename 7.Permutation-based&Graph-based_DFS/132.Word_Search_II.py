a0742da7b1fd56cf4479e5b3ae65cabd86d21037
fudigit <30851222+fudigit@users.noreply.github.com>
2019-04-14 11:11:59 -0500

    Update 132.Word_Search_II.py

--- 7.Permutation-based&Graph-based_DFS/132.Word_Search_II.py
+++ 7.Permutation-based&Graph-based_DFS/132.Word_Search_II.py
@@ -1,27 +1,33 @@
 '''
-error:
-1. Capitalize contstant!
-2. convert result to list
-3. not in visited or in visited
-4. not in board or in board
-5. string has not pop
-6, visited starts with {(i, j)}
-7. when added a word to result, return not required, since long one may exists
-8, do next dfs on x_, y_, not x, y!
+use dfs + hash table
+# 1. construct a prefix dict to check if the itermediate search result is on the right path
+# 2. do search for every char in the board
+# 3. if the path starts with the char exists in prefix, continue search in all directions
+In gnenral, time complexity is hard to determine for search problem
 '''
 
+'''
+errors:
+1. Capitalize contstant!
+2. convert result (set()) to list
+3. not in visited or in visited? If in, skip, because on char can be used once 
+4. not in board or in board
+5. string has not pop
+6, visited starts with {(i, j)}, the position of the initial char
+7. when added a word to result, return not required, since longer word may exists
+8, do next dfs on x_, y_, not x, y!
+'''
+DIRECTIONS = [(1,0), (0,-1), (-1,0), (0,1)] # definine as constant outside of class
 class Solution:
     """
     @param board: A list of lists of character
     @param words: A list of string
     @return: A list of string
     """
+
     def wordSearchII(self, board, words):
-        self.next_move = [(1,0), (0,-1), (-1,0), (0,1)] 
-        results = set()
         
-        # 
-        # construct a set of prefix, as well as the word itself, can be used in search
+        # construct a set of prefix, including the word itself, if path not in prefix, break
         words_dict = set(words) # make the search time O(1)
         prefix_dict = set()
         for word in words_dict:
@@ -29,26 +35,30 @@ class Solution:
                 prefix_dict.add(word[:i + 1])
 
         # start from each position of the matrix, use dfs to find the word
+        results = set()
+        
         for i in range(len(board)):
             for j in range(len(board[0])):
                 char = board[i][j]
-                #print(char)
                 self.dfs_findPath(board, i, j, char, words_dict, prefix_dict, set([(i,j)]), results)
                 
         return list(results) # convert since results is a set!
         
         
-        # use dfs to find path of word
+        # find path starting from word with path in the dict, add the path found into the results
     def dfs_findPath(self, board, x, y, word, words_dict, prefix_dict, visited, results):
+        # exist
         if word in words_dict:
-            results.add(word)
+            results.add(word)      #longer word may exist, do not return
         
         if word not in prefix_dict:
             return
-            
-        for x_move, y_move in self.next_move:
+        
+        # increase the path by 1 step on each possible directions
+        for x_move, y_move in DIRECTIONS:
             x_, y_ = x + x_move, y + y_move
-                
+            
+            # check if the next position is not visied, and in board
             if (x_, y_) in visited:
                 continue
             if not self.isInBoard(board, x_, y_):
@@ -56,12 +66,12 @@ class Solution:
                 
             visited.add((x_, y_))
             #word_added = word + board[x_][y_]
-            #print(board[x_][y_],x_, y_,'hey', visited, word + board[x_][y_])
+            #print(board[x_][y_],x_, y_,'sub dfs', visited, word + board[x_][y_])
             self.dfs_findPath(board, x_, y_, word + board[x_][y_], words_dict, prefix_dict, visited, results)
             #word_added.pop()
             visited.remove((x_, y_))
                 
-        # make sure x, y is in boundary 
+        # make sure x, y is in bound 
     def isInBoard(self, board, x, y):
         n, m = len(board), len(board[0])
         if 0 <= x < n and 0 <= y < m:
