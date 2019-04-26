edbed5b86e0d0f4b66d045206346de2995580fdd
fudigit <30851222+fudigit@users.noreply.github.com>
2019-04-26 00:04:10 -0500

    Create 627.Longest_Palindrome.py

---
+++ 1.String/627.Longest_Palindrome.py
@@ -0,0 +1,26 @@
+class Solution:
+    """
+    @param s: a string which consists of lowercase or uppercase letters
+    @return: the length of the longest palindromes that can be built
+    """
+    ###count how many times each letter occurs, even will be palindromes, odd > 1 will give a count - 1 palindrome. Add 1 final count if odd or single has occurred 
+    '''O(n) + O(len(letter_dic) + O(1)'''
+    ### use dictionary to count how many times each letter appeared
+    def longestPalindrome(self, s):
+        check_set = []
+        
+        for i in s:
+            if i not in check_set:
+                check_set.append(i)
+            elif i in check_set:
+                check_set.remove(i)
+                
+        if len(check_set) == 0:
+            length = len(s)
+        elif len(check_set) >= 1:
+            length = len(s) - len(check_set) + 1
+        
+        return length
+            
+            
+        # write your code here
