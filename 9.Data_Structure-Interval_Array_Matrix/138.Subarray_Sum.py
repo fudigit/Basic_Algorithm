a088d895e45ca1e2bb5d7f936bd494b4eaeead6c
fudigit <30851222+fudigit@users.noreply.github.com>
2019-04-28 21:31:13 -0500

    Create 138.Subarray_Sum.py

---
+++ 9.Data_Structure-Interval_Array_Matrix/138.Subarray_Sum.py
@@ -0,0 +1,34 @@
+'''
+use prefix + hashmap
+1. record the prefix_sum and its index that has first encountered
+2. if a second encounter for the same prefix_sum, interval of sum 0 has found!
+
+prefix_sum[i] = A[0] + A[1] + .. + A[i]
+sum(i, j) = 0 = prefix[j] - prefix[i-1]
+=> prefix[j] = prefix[i-1]
+sum(i, j) means finding 2 prefix_sum has equal value
+
+使得prefix_sum包含A[i],会让思考简单一些
+
+TC: O(n)
+'''
+
+class Solution:
+    """
+    @param nums: A list of integers
+    @return: A list of integers includes the index of the first number and the index of the last number
+    """
+    def subarraySum(self, nums):
+        
+        prefix_sum = 0
+        prefixToIndex = {0:-1}
+        
+        for i in range(len(nums)):
+            prefix_sum = prefix_sum + nums[i] # prefix is nums[0] + nums[1] + ... + nums[j]
+            # print(j,prefixToIndex, prefix)
+            if prefix_sum not in prefixToIndex:
+                prefixToIndex[prefix_sum] = i
+            elif prefix_sum in prefixToIndex:
+                return prefixToIndex[prefix_sum] + 1, i
+
+        return -1, -1
