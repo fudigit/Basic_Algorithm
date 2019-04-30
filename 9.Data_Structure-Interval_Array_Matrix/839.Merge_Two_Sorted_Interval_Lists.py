235b588be4c068daa3fa57b992533f2e1e383e40
fudigit <30851222+fudigit@users.noreply.github.com>
2019-04-30 12:21:39 -0500

    Update 839.Merge_Two_Sorted_Interval_Lists.py

--- 9.Data_Structure-Interval_Array_Matrix/839.Merge_Two_Sorted_Interval_Lists.py
+++ 9.Data_Structure-Interval_Array_Matrix/839.Merge_Two_Sorted_Interval_Lists.py
@@ -1,5 +1,5 @@
 '''
-1. add 2 lists together and sort by interval.start. O(Nlog(N))
+1. add 2 lists together and sort by interval.start. O((n+m)*log(n+m))
 2. loop through the list and merge the intervals
 - not utilizing the sorted(ascending) intervals
 - try to do it with merge 2 sorted array
@@ -39,3 +39,57 @@ class Solution:
                 ans[-1].end = max(item.end, ans[-1].end)
         
         return ans
+    
+ 
+'''
+O(log(n + m))
+
+since the 2 list of intervals are already sorted, similar to merge 2 sorted array
+difference:
+- instead appending the smaller to the merged array, now adding the interval to the list
+- the interval to be added can/can't intersect with the last interval, therefore creat a interval add function
+
+'''
+
+"""
+Definition of Interval.
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    """
+    @param list1: one of the given list
+    @param list2: another list
+    @return: the new sorted list of interval
+    """
+    def mergeTwoInterval(self, list1, list2):
+        
+        new = []
+        i, j = 0, 0
+        while i < len(list1) and j < len(list2):
+            if list1[i].start <= list2[j].start:
+                self.add_interval(new, list1[i])
+                i += 1
+            else:
+                self.add_interval(new, list2[j])
+                j += 1
+        while i < len(list1):
+            self.add_interval(new, list1[i])
+            i += 1
+        while j < len(list2):
+            self.add_interval(new, list2[j])
+            j += 1
+        return new
+        
+    def add_interval(self, new, interval):
+        if new == []:
+            new.append(interval)
+            return
+        last = new[-1]
+        if interval.start > last.end:
+            new.append(interval)
+            return
+        last.end = max(interval.end, last.end)
