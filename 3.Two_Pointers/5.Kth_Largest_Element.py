08ef6f716e2cbcaf5976513d215101705b2093ac
fudigit <30851222+fudigit@users.noreply.github.com>
2019-05-01 00:01:24 -0500

    Update 5.Kth_Largest_Element.py

--- 3.Two_Pointers/5.Kth_Largest_Element.py
+++ 3.Two_Pointers/5.Kth_Largest_Element.py
@@ -1,6 +1,6 @@
 '''
 Use the right end of interval as reference point for k
-1. sort the array in asending order
+1. sort the array partially in asending order
 2. use interval's right end as the reference point for k. (k = 1, index = len(A) - 1  
 3. return the actual value once kth largest is found
 4. len(A) - k is the index of kth largest number
