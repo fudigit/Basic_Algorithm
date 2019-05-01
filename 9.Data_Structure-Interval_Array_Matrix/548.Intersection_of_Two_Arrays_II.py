db5ecde0a1b6906c35c9ce18ea29ccddb834ad3a
fudigit <30851222+fudigit@users.noreply.github.com>
2019-04-30 23:32:02 -0500

    Update 548.Intersection_of_Two_Arrays_II.py

--- 9.Data_Structure-Interval_Array_Matrix/548.Intersection_of_Two_Arrays_II.py
+++ 9.Data_Structure-Interval_Array_Matrix/548.Intersection_of_Two_Arrays_II.py
@@ -30,3 +30,26 @@ class Solution:
                 nums1ToCount[num] -= 1
                 res.append(num)
         return res
+
+    
+class Solution:
+    """
+    @param nums1: an integer array
+    @param nums2: an integer array
+    @return: an integer array
+    """
+    def intersection(self, nums1, nums2):
+        nums1.sort()
+        nums2.sort()
+        
+        i, j = 0, 0
+        res = []
+        while i < len(nums1) and j < len(nums2):
+            if nums1[i] == nums2[j]:
+                res.append(nums1[i])
+                i += 1
+                j += 1
+            elif nums1[i] < nums2[j]:
+                i += 1 
+            else:
+                j += 1
