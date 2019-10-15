bde031f6b7b282035d04d2966bb8e7da2619e976
fudigit <30851222+fudigit@users.noreply.github.com>
2019-10-15 11:33:56 -0500

    Update 13.Implement_strStr().py

--- 1.String/13.Implement_strStr().py
+++ 1.String/13.Implement_strStr().py
@@ -1,3 +1,8 @@
+
+'''
+Not a good solution: 1. changing i in the for loop. 2. need to take care of target = 's' specifically
+'''
+
 class Solution:
     """
     @param source: 
@@ -5,16 +10,16 @@ class Solution:
     @return: return the index
     """
     def strStr(self, source, target):
+    
+        if target == '':
+            return 0
         
-        for i in range(len(source)):
+        for i in range(len(source) - len(target) + 1):
             j = 0
             while source[i] == target[j] and j < len(target) - 1:
                 i += 1
                 j += 1
-            if j == len(target):
-                return i - len(target) + 1
-            else:
-                pass
+            if j == len(target) - 1 and source[i] == target[j]:
+                return i - j
             
         return -1
-            
