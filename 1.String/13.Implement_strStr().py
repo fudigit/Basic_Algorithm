ae1630cd4af0b47a077942f2f8289dc113e09f83
fudigit <30851222+fudigit@users.noreply.github.com>
2019-10-15 12:00:28 -0500

    Update 13.Implement_strStr().py

--- 1.String/13.Implement_strStr().py
+++ 1.String/13.Implement_strStr().py
@@ -1,3 +1,33 @@
+'''
+use i + j to track the char in source, i is the initial index
+'''
+
+class Solution:
+    """
+    @param source: 
+    @param target: 
+    @return: return the index
+    """
+    def strStr(self, source, target):
+    
+        if target == '':
+            return 0
+        
+        source_len, target_len = len(source), len(target)
+        
+        for i in range(source_len - target_len + 1):
+            j = 0
+            while j < target_len:
+                if source[i + j] != target[j]:
+                    break
+                else:
+                    j += 1
+            
+            if j == target_len:
+                return i
+            
+        return -1
+
 
 '''
 Not a good solution: 1. changing i in the for loop. 2. j < len(target) - 1， 意味着target的最后一个元素没有比较，（只到倒数第二个）
