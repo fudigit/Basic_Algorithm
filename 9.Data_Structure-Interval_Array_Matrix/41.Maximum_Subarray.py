8479a8a818b1dc9903fa8c8ad560101429e56f9e
fudigit <30851222+fudigit@users.noreply.github.com>
2019-10-18 17:50:29 -0500

    Update 41.Maximum_Subarray.py

--- 9.Data_Structure-Interval_Array_Matrix/41.Maximum_Subarray.py
+++ 9.Data_Structure-Interval_Array_Matrix/41.Maximum_Subarray.py
@@ -1,4 +1,3 @@
-# 拿到题目没印象了，二刷
 '''
 1,2,3,6,8,9,0,-5,6
 |   |     | |
@@ -34,4 +33,40 @@ class Solution:
         return max_sub
         
         
+'''
+2刷，拿到题目没印象了
+
+# 构造一个 prefix sum
+# prefix sum is defined as prefix[i] = nums[0] + ..., nums[i-1], where prefix[0] = 0
+# then we can write Sum(i ~ j) as prefix[j+1] - prefix[i]
+# 前j+1个数之和，减去前i个数之和，就是下标为i到下标为j的数之和。
+
+# 使用前缀和的方法，计算每个位置为结尾的subarray的最大值
+# 前缀和减去前缀和最小，得到subarray最大。
+'''
+class Solution:
+    """
+    @param nums: A list of integers
+    @return: A integer indicate the sum of max subarray
+    """
+    def maxSubArray(self, nums):
+        
+        if nums is None or nums is []:
+            return 0
+            
+        prefix_sum = 0
+        # 记录前缀和
+        max_sum = - sys.maxsize
+        # 记录全局最大
+        prefix_min_sum = 0
+        # 记录前i个数中(nums[:i])的最小前缀和sum(0 ~ k), where k <= i
+        
+        #注意前缀和最小是后更新的，否则会重合
+        for num in nums:
+            prefix_sum = prefix_sum + num
+            max_sum = max(prefix_sum - prefix_min_sum, max_sum)
+            prefix_min_sum = min(prefix_min_sum, prefix_sum)
+
+        return max_sum
+            
         
