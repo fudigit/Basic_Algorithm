96015073305fd9b362cfee03456499a027f28113
fudigit <30851222+fudigit@users.noreply.github.com>
2019-10-21 11:42:00 -0500

    Update 138.Subarray_Sum.py

--- 9.Data_Structure-Interval_Array_Matrix/138.Subarray_Sum.py
+++ 9.Data_Structure-Interval_Array_Matrix/138.Subarray_Sum.py
@@ -58,3 +58,32 @@ class Solution:
             elif prefix in prefix_dict:
                 print(prefix_dict)
                 return [prefix_dict[prefix], i + 1 - 1]
+            
+            
+'''
+3刷，定义prefix(j) = A[0] + ... + A[j]。注：与九章定义不同，九章为前j个数，这里是前j + 1个数。这个定义更直观
+# 坐标为i至j的数的和, sum(i~j) = 0  ==>  prefix(j) - prefix(i-1) = 0 ，注意，保留A[i]!
+# 反之，如果prefix(j) - prefix(k) = 0, 说明sum(k+1, j) = 0
+# 建立一个value to index的mapping，从左往右边记录prefix_sum，去找j和k，such that prefix(j) - prefix(k) = 0
+# - 说明sum(k+1, j) = 0
+'''
+
+class Solution:
+    """
+    @param nums: A list of integers
+    @return: A list of integers includes the index of the first number and the index of the last number
+    """
+    def subarraySum(self, nums):
+        valueToIndex = {}
+        valueToIndex[0] = -1 #prefix(-1) = 0, A[-1] = None
+        
+        prefix_sum = 0
+        for i in range(len(nums)):
+            prefix_sum += nums[i]
+            if prefix_sum not in valueToIndex:
+                valueToIndex[prefix_sum] = i
+            elif prefix_sum in valueToIndex:
+                # the ask is the index of the 1st and last num in sub, not to print the subarray
+                return [valueToIndex[prefix_sum] + 1, i]
+                
+        return 'no subarray found'
