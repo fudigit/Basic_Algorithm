5bde8d45f8885eae4625d1bb3dd071dabc3bbdc6
fudigit <30851222+fudigit@users.noreply.github.com>
2019-10-23 23:59:04 -0500

    Update 627.Longest_Palindrome.py

--- 1.String/627.Longest_Palindrome.py
+++ 1.String/627.Longest_Palindrome.py
@@ -26,3 +26,16 @@ class Solution:
         # write your code here
 
        # new solution
+
+class Solution:
+    """
+    @param s: a string which consists of lowercase or uppercase letters
+    @return: the length of the longest palindromes that can be built
+    """
+    def longestPalindrome(self, s):
+        
+        char_odd = {}
+        
+        for char in s:
+            if char in char_odd:
+                
