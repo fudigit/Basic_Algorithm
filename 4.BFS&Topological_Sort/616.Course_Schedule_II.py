1fda7dba7361511c2d5bd1ab84eaac091252d0d2
fudigit <30851222+fudigit@users.noreply.github.com>
2019-10-30 17:59:17 -0500

    Update 616.Course_Schedule_II.py

--- 4.BFS&Topological_Sort/616.Course_Schedule_II.py
+++ 4.BFS&Topological_Sort/616.Course_Schedule_II.py
@@ -42,3 +42,43 @@ class Solution:
         if len(order) == numCourses:
             return order
         return []
+
+    
+# 二刷，拓扑排序的定义我忘了。要先把图变转化为indegree和neighbor
+from collections import deque
+class Solution:
+    """
+    @param: numCourses: a total of n courses
+    @param: prerequisites: a list of prerequisite pairs
+    @return: the course order
+    """
+    def findOrder(self, numCourses, prerequisites):
+        # course to indegrees: {0:0, 1:1, 2:1, 3:2}
+        course_indegrees = {i:0 for i in range(numCourses)} #how many dependencies
+        for pair in prerequisites:
+            course_indegrees[pair[0]] += 1
+        
+        # preq course to current course: {0:[1,2], 1:[3], 2:[3]}
+        neighbors = {i:[] for i in range(numCourses)}
+        for cur, pre in prerequisites:
+            neighbors[pre].append(cur)
+        
+        # add those course that has indegree of 0 to queue
+        start_nodes = [c for c in course_indegrees if course_indegrees[c] == 0]
+        queue = collections.deque(start_nodes)
+        order = []
+        
+        # kill the course with indegree of 0, which reduce the indgree of the next courses by 1
+        while queue:
+            course = queue.popleft()
+            order.append(course)
+            
+            for next_course in neighbors[course]:
+                course_indegrees[next_course] -= 1 
+                if course_indegrees[next_course] == 0:
+                    queue.append(next_course)
+            
+        if len(order) == numCourses:
+            return order
+        
+        return []
