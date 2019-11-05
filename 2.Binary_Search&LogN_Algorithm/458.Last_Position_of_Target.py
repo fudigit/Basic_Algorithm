9cabecf9d9452933751ea74792199ceda0aaee38
fudigit <30851222+fudigit@users.noreply.github.com>
2019-11-05 17:39:07 -0600

    Update 458.Last_Position_of_Target.py

--- 2.Binary_Search&LogN_Algorithm/458.Last_Position_of_Target.py
+++ 2.Binary_Search&LogN_Algorithm/458.Last_Position_of_Target.py
@@ -26,3 +26,33 @@ class Solution:
             return s
         else:
             return -1
+
+
+# 2刷
+class Solution:
+    """
+    @param nums: An integer array sorted in ascending order
+    @param target: An integer
+    @return: An integer
+    """
+    def lastPosition(self, nums, target):
+        if nums == []:
+            return -1
+        
+        start = 0
+        end = len(nums) - 1
+        
+        while start + 1 < end:
+            mid = start + (end - start)//2
+            if nums[mid] > target:
+                end = mid
+            else:
+                start = mid
+                
+        if nums[end] == target:
+            return end
+        if nums[start] == target:
+            return start
+        
+        return -1
+        
