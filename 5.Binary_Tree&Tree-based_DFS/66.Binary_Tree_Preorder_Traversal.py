f18d97a0dfea0955e7e181d0fb6bff806db7374a
fudigit <30851222+fudigit@users.noreply.github.com>
2019-11-07 18:16:57 -0600

    Update 66.Binary_Tree_Preorder_Traversal.py

--- 5.Binary_Tree&Tree-based_DFS/66.Binary_Tree_Preorder_Traversal.py
+++ 5.Binary_Tree&Tree-based_DFS/66.Binary_Tree_Preorder_Traversal.py
@@ -18,10 +18,44 @@ class Solution:
         self.traverse(root)
         return self.result
         
-        
     def traverse(self, node):
         if node is None:
             return
         self.result.append(node.val)
         self.traverse(node.left)
         self.traverse(node.right)
+
+        
+        
+"""
+Definition of TreeNode:
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
+"""
+class Solution:
+    """
+    @param root: A Tree
+    @return: Preorder in ArrayList which contains node values.
+    """
+    def preorderTraversal(self, root):
+        stack = []
+        preorder = []
+        
+        if not root:
+            return preorder
+        
+        stack.append(root)
+        
+        while stack != []:
+            node = stack.pop()
+            val = node.val
+            preorder.append(val)
+            
+            if node.right:
+                stack.append(node.right)
+            if node.left:
+                stack.append(node.left)
+                
+        return preorder
