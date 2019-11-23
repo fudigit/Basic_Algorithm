410bceaf914a04c5fa6726ab3f30e65e782f3622
fudigit <30851222+fudigit@users.noreply.github.com>
2019-11-22 22:44:19 -0600

    Update 76.Longest_Increasing_Subsequence.py

--- 9.10.Dynamic_Programming/76.Longest_Increasing_Subsequence.py
+++ 9.10.Dynamic_Programming/76.Longest_Increasing_Subsequence.py
@@ -18,3 +18,30 @@ class Solution:
                     dp[cur] = max(dp[prev] + 1, dp[cur])
         print(dp)
         return max(dp)
+    
+    
+    class Solution:
+    """
+    @param nums: An integer array
+    @return: The length of LIS (longest increasing subsequence)
+    """
+    def longestIncreasingSubsequence(self, nums):
+        
+        # 以第i个数字为结尾的最长上升子序列
+        # 是否可以推导出以第i+1个数字为结尾的最长上升子序列？
+        # 如果知道第1 ~ i的LIS, 求i+1？看第i+1个数字是否继续递增，取最长
+        
+        if not nums:
+            return 0
+            
+        dp = [1 for i in range(len(nums))]
+        
+        for cur, val in enumerate(nums):
+            tmp = 0
+            for prev in range(cur):
+                if val > nums[prev]:
+                    tmp = max(tmp, dp[prev])
+            tmp += 1
+            dp[cur] = tmp
+                    
+        return max(dp)
