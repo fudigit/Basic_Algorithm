9ce6da09445704c9dd6ed38bfc7ea15708d4d668
fudigit <30851222+fudigit@users.noreply.github.com>
2019-11-26 16:15:30 -0600

    Update 35.Reverse_Linked_List.py

--- 1.String/35.Reverse_Linked_List.py
+++ 1.String/35.Reverse_Linked_List.py
@@ -7,37 +7,6 @@ class ListNode(object):
         self.val = val
         self.next = next
 """
-
-class Solution:
-    """
-    @param head: n
-    @return: The new head of reversed linked list.
-    """
-    def reverse(self, head):
-        
-        prev = head
-        cur = head.next
-        
-        while cur.val
-            next_node = cur.next
-            cur.next = prev
-            prev = cur
-            cur = next_node
-            
-        head.next = None
-        
-        return
-        
-
-"""
-Definition of ListNode
-
-class ListNode(object):
-
-    def __init__(self, val, next=None):
-        self.val = val
-        self.next = next
-"""
 # 实际上移动的就是prev和cur, after的作用是把下一个node拿到，否则cur → prev以后，就拿不到下一个node
 # 指针是在reference node，然后把node之间的指向做出改变
 # 注意最后停下来，看的是cur变成了None。但表头则变成了prev
