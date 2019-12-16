86ecd32ea04aa1ee20f32697693b177c5509f1c0
fudigit <30851222+fudigit@users.noreply.github.com>
2019-12-15 23:07:01 -0600

    Update 2.Trailing_Zeros.py

--- B1.Mathematics/2.Trailing_Zeros.py
+++ B1.Mathematics/2.Trailing_Zeros.py
@@ -1,3 +1,27 @@
+# v1 
+class Solution:
+    """
+    @param: n: An integer
+    @return: An integer, denote the number of trailing zeros in n!
+    """
+    def trailingZeros(self, n):
+        res = 0
+        
+        while n >= 5:
+            res += n//5
+            n = n//5
+        
+        return res
+        
+        
+        # trailing zero总数 = n//5 + n//25 + n//125 + n//625 + n//3125 + ...
+        # 为什么这就是尾部0的总数？出现0，需要出现2*5，而2的数量比5多
+        # 实际上统计5这个因子出现的总个数
+
+
+
+
+# v.brute_force, O(n), does not pass!
 class Solution:
     """
     @param: n: An integer
